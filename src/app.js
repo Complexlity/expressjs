64975d211106d8eb3d37d851233809f386154d77
Complexlity <nwalozielijah@gmail.com>
2024-02-09 12:09:47 +0100

    chore: add prove route

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@ import morgan from "morgan";
 
 import * as middleware from "./utils/middleware.js";
 import helloRoute from "./routes/helloRouter.js";
+import { BarretenbergBackend } from "@noir-lang/backend_barretenberg";
+import { Noir } from "@noir-lang/noir_js";
+import noir_program from "./circuit/target/hello.json" assert { type: "json" };
+import express from "express";
+
+
 
 const app = express();
 
@@ -21,7 +27,23 @@ app.get("/", (req, res) => {
   res.status(200).send({ status: "ok" });
 });
 
-app.use("/hello", helloRoute);
+
+app.get("/proof", async (req, res) => {
+  console.log({ noir_program });
+
+  const inputs = { x: 1, y: 2 };
+  const backend = new BarretenbergBackend(noir_program);
+  const program = new Noir(noir_program, backend);
+
+  console.log("Getting proof data...");
+  const proofData = await program.generateFinalProof(inputs);
+  const verifiedProof = await program.generateFinalProof(inputs);
+
+  console.log("done");
+  console.log({ proofData });
+  res.json({ proofData, verifiedProof });
+});
+
 
 // custom middleware
 app.use(middleware.unknownEndpoint);
