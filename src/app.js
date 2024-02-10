f28ada40644271d3f937aef02f438521585a4ad2
Complexlity <nwalozielijah@gmail.com>
2024-02-10 12:07:32 +0100

    feat: add sudoku proofing

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import morgan from "morgan";
 
 import { BarretenbergBackend } from "@noir-lang/backend_barretenberg";
 import { Noir } from "@noir-lang/noir_js";
-import noir_program from "../circuit/target/hello.json" assert { type: "json" };
+import noir_program from "../circuit/target/sudoku.json" assert { type: "json" };
 import * as middleware from "./utils/middleware.js";
 
 
@@ -25,21 +25,28 @@ app.get("/", (req, res) => {
   res.status(200).send({ status: "ok" });
 });
 
+app.post("/proof", async (req, res) => {
 
-app.get("/proof", async (req, res) => {
-  console.log({ noir_program });
+  const solutionString = req.body.solution
+  console.log({solutionString})
+  const solution = solutionString.split("").map(Number)
+  console.log({solution})
 
-  const inputs = { x: 1, y: 2 };
   const backend = new BarretenbergBackend(noir_program);
   const program = new Noir(noir_program, backend);
 
   console.log("Getting proof data...");
-  const proofData = await program.generateFinalProof(inputs);
-  const verifiedProof = await program.verifyFinalProof(proofData);
+  try {
+    const proofData = await program.generateFinalProof({solution});
+    const verifiedProof = await program.verifyFinalProof(proofData);
 
-  console.log("done");
-  console.log({ proofData });
-  res.json({ proofData, verifiedProof });
+    console.log("done");
+    console.log({ proofData });
+
+    return res.status(200).json({ proofData, verifiedProof });
+  } catch (error) {
+    return res.status(500).json({error: "Incorrect Solutino"})
+  }
 });
 
 
