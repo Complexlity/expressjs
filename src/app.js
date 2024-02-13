19e0aad91f398b890f9a0feddd4050f672b23050
Complexlity <nwalozielijah@gmail.com>
2024-02-13 09:59:10 +0100

    chore: clean code

--- src/app.js
+++ src/app.js
@@ -22,13 +22,6 @@ app.use(cors());
 // request logger middleware
 app.use(morgan("tiny"));
 
-const solution = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 4, 5, 6, 7, 8, 9, 1, 2, 3, 7, 8, 9, 1, 2, 3, 4, 5,
-  6, 2, 1, 4, 3, 6, 5, 8, 9, 7, 3, 6, 5, 8, 9, 7, 2, 1, 4, 8, 9, 7, 2, 1, 4, 3,
-  6, 5, 5, 3, 1, 6, 4, 2, 9, 7, 8, 6, 4, 2, 9, 7, 8, 5, 3, 1, 9, 7, 8, 5, 3, 1,
-  6, 4, 2,
-];
-
 // healthcheck endpoint
 app.get("/", (req, res) => {
   res.status(200).send({ status: "ok" });
